154187b96762bda1ba32cd371df74ee767582462
eje543 <ejelliott1@my.waketech.edu>
2025-05-01 15:25:24 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -53,6 +53,5 @@ function populateGradebook(data) {
 
 // TODO REMOVE THIS
 // Call the stubs to demonstrate the workflow
-const gradeData = fetchGradeData();
-populateGradebook(gradeData);
+fetchGradeData();
 // END REMOVE
